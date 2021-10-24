1ae11a2ebd3dccdd3308d9f1df9da6ef3439687d
Bob goodwin <goodw21985@live.com>
2021-10-24 16:25:09 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AetherScriptsForL15Trainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
